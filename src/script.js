870cddd7cebbaeeccc66f68f2a70ffdfaa0f37c2
kdinesh1234 <158142344+kdinesh1234@users.noreply.github.com>
2024-09-03 08:02:02 -0700

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,20 @@
+const mainDiv = document.getElementById("mainDiv");
+const getDetails = document.getElementById("getDetails");
+
+
+getDetails.addEventListener("click", async () => {
+  //!using the universities api to fetch the data from server.
+  const res1 = await fetch(
+    "https://yesno.wtf/api"
+  );
+  const data1 = await res1.json();
+  console.log(data1);
+    const output1 = document.getElementById("api1");
+    if(data1.answer==="yes"){
+    output1.innerHTML  = ` <strong>👌👍 YES 👍👌 </strong><br> <img src="${data1.image}"height="350px" alt="Yes / No">`;
+    }
+    else{
+      output1.innerHTML  = `<strong> 👎👎NO 👎👎</strong><br><img src="${data1.image}"height="350px" alt="Yes / No">`;
+    }
+
+});
